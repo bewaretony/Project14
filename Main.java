2922bf68474f55445a8cc2f9475792c678a4f1bd
Anthony Luo <[www.anthonyluo@gmail.com]>
2015-12-18 13:38:46 -0700

    changed main and room

--- Main.java
+++ Main.java
@@ -8,26 +8,24 @@
 public class Main
 {
     // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Main
-     */
-    public Main()
-    {
-        // initialise instance variables
-        x = 0;
+    
+    public static void main(String[] args) {
+        
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y + 50;
+    public static void addScore(int s) {
+        
+        
+    }
+    public static void readCommand(String command) {
+        
+    }
+    public static Room currentRoom() {
+        
+    }
+    public static Player location() {
+        
+    }
+    public static int score() {
+        
     }
 }
--- Room.java
+++ Room.java
@@ -7,27 +7,13 @@
  */
 public class Room
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Room
-     */
-    public Room()
-    {
-        // initialise instance variables
-        x = 0;
+    public Room(Item[]s, Character[] p, String n, String d) {
+        
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public String getDescription() {
+        
     }
-}
+    
+    public 
+    }
