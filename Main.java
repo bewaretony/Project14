3b3d781019fe219757be8348bd4463b13be3281c
Anthony Luo <www.anthonyluo@gmail.com>
2015-12-19 19:46:16 -0700

    more main method changes

--- Main.java
+++ Main.java
@@ -29,20 +29,29 @@ public class Main
         
     }
     public static void setup() {
+       //spawn
         Item[] spawnItems = new Item[1];
         Character[] spawnCharacter = new Character[0];
-        spawnItems[0] = new Item("book", "");
+        spawnItems[0] = new Item("book", "Welcome! My name is the Innkeeper and I am here to guide ");
         Room spawn = new Room(spawnItems, spawnCharacter, "Spawn point", "You spawn in forest. Ahead of you there is a clearing, and in the distance you can see a ruined castle. You see a tattered book lying along the road");
+        //clearing next to spawn
         Item[] clearingItems = new Item[0];
         Character[] cspawnItems = new Character[0];
         Room clearingSpawn = new Room(clearingItems, cspawnItems, "Clearing", "You are in a clearing in the middle of a gorge. You see cave to your right");
-        Item[2] ecastleItems = new Item[2];
-        ecastleItems[0]= new Item("book"
+       //cave room 
+        Item[2] caveItems = new Item[2];
+        caveItems[0]= new Item("key", "This is the key to the castle");
+        caveItems[1]= new Item("stones", "some random stones");
+        Character[] caveCharacters = new Character[0];
+        Room cave = new Room(caveItems, caveCharacters, "Cave in the Gorge", "You enter a dark cave. You see a pile of stones on the left and a shiny object on the right");
+        //castle entrance
+        
         Room castleEntrance = new Room();
         spawn.setNorth(clearingSpawn);
         clearingSpawn.setSouth(spawn);
         castleEntrance.setSouth(clearingSpawn);
         clearingSpawn.setNorth(castleEntrance);
+        
         
     }
 }
